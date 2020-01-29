2850d9c230e7efdf7690dcf624d583037ebde3f4
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-01-29 19:03:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,20 +3,25 @@ class Dog
   def name=(name)
     
     @name=name
+    
   end
   
   def name
+    
     @name
     
   end
   
-  def breed =(breed)
+  def breed=(breed)
     
     @bredd = breed
     
   end
+  
   def breed
+    
     @breed
+    
   end
   
   
